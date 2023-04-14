e454d93378e416a653fcb27f8957f1cfbb604d21
yash jangra <yashjangra617@gmail.com>
2023-04-14 13:36:17 +1000

    Update server.js

--- server.js
+++ server.js
@@ -8,25 +8,25 @@ app.use(express.urlencoded({ extended: false }));
 const cardList = [
     {
         title: "Kitten 2",
-        image: "images/kitten-2.jpg",
+        image: "images/kitten-2.png",
         link: "About Kitten 2",
         desciption: "Demo desciption about kitten 2"
     },
 
     {
         title: "Kitten 3",
-        image: "images/kitten-3.jpg",
+        image: "images/kitten-3.png",
         link: "About Kitten 3",
         desciption: "Demo desciption about kitten 3"
     }
 ]
 
-app.get('/api/projects',(req,res) => {
+app.get('/api/cats',(req,res) => {
     res.json({statusCode: 200, data: cardList, message:"Success"})
 })
 
 var port = process.env.port || 3000;
 
 app.listen(port,()=>{
-    console.log("App listening to: "+port)
+    console.log("App listening to: "+port);
 })
