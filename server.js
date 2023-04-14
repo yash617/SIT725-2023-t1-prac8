046bbc6aa9bb1537c904a9653aec69af6b78e9bf
yash jangra <yashjangra617@gmail.com>
2023-04-14 14:51:58 +1000

    Update server.js

--- server.js
+++ server.js
@@ -1,32 +1,62 @@
 var express = require("express")
 var app = express()
+const {MongoClient} = require('mongodb');
+const uri = 'mongodb+srv://admin:admin@cluster0.ggzuodr.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(uri);
+let dbCollection;
 
 app.use(express.static(__dirname+'/public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const cardList = [
-    {
-        title: "Kitten 2",
-        image: "images/kitten-2.png",
-        link: "About Kitten 2",
-        desciption: "Demo desciption about kitten 2"
-    },
-
-    {
-        title: "Kitten 3",
-        image: "images/kitten-3.png",
-        link: "About Kitten 3",
-        desciption: "Demo desciption about kitten 3"
-    }
-]
-
 app.get('/api/cats',(req,res) => {
     res.json({statusCode: 200, data: cardList, message:"Success"})
 })
 
+function dbConnection(collectionName) {
+        client.connect(err => {
+        dbCollection = client.db().collection(collectionName);
+        if (!err) {
+            console.log('DB Connected');
+            console.log(dbCollection);
+        } else {
+            console.log(err);
+        }
+    });
+}
+
+app.post('/api/cats', (req, res) => {
+    let cat = req.body;
+    insert(cat, (err, result) => {
+        if (err) {
+            res.json({statusCode:400, message: err});
+        } else {
+            res.json({statusCode: 200, data: result, message: 'Cat successfully added'});
+        }
+    });
+});
+
+app.get('/api/cats',(req,res) => {
+    getAllCats((error, result) => {
+        if (error) {
+            res.json({statusCode:400, message: err});
+        } else {
+            res.json({statusCode: 200, data: result, message: 'Successfully'});
+        }
+    });
+});
+
+function insert(cat, callback) {
+    dbCollection.insertOne(cat, callback);
+}
+
+function getAllCats(callback) {
+    dbCollection.find().toArray(callback);
+}
+
 var port = process.env.port || 3000;
 
 app.listen(port,()=>{
-    console.log("App listening to: "+port);
+    console.log('App listening to: ' + port);
+    dbConnection('Cats');
 })
