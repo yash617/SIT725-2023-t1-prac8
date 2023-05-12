b8b614473c1251f8cbd57bfd6f7eddcc292f04df
yash jangra <54751099+yash617@users.noreply.github.com>
2023-05-12 16:48:49 +1000

    Update route.js

--- route/route.js
+++ route/route.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var router = express.Router();
-let controller = require('../controller/controller');
+let controller = require('../contoller/controller');
 
 router.post('/api/cats', (req, res) => {
-    controller.insertCat(req,res);
+    controller.insertCat(req, res);
 });
 
-router.get('/api/cats',(req,res) => {
-    controller.getAllCats(req,res);
+router.get('/api/cats', (req, res) => {
+    controller.getAllCats(req, res);
+});
+
+router.delete('/api/cats', (req,res) => {
+    controller.deleteCat(req,res);
 });
 
 module.exports = router;
