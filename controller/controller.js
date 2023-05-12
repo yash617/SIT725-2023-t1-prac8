db5085e6765396fec0a07614986b332f7894e101
yash jangra <54751099+yash617@users.noreply.github.com>
2023-05-12 16:51:35 +1000

    Update controller.js

--- controller/controller.js
+++ controller/controller.js
@@ -1,24 +1,35 @@
 let model = require('../model/model');
 
-const insertCat = (req,res) => {
-    let cat = req.body;
-    model.insertCat(cat, (err, result) => {
-        if (err) {
-            res.json({statusCode: 400, message: err});
+const insertCat = (req, res) => {
+    let newProject = req.body;
+    model.insertCat(newProject, (error, result) => {
+        if (error) {
+            res.json({statusCode: 400, message: error});
         } else {
-            res.json({statusCode: 200, data: result, message: 'Cat Successfully Added'});
+            res.json({statusCode: 200, data: result, message: 'project successfully added'});
         }
     });
-}
+};
 
 const getAllCats = (req, res) => {
-    model.getAllCats((err, result) => {
-        if (err) {
-            res.json({statusCode: 400, message: err});
+    model.getCats((error, result) => {
+        if(error) {
+            res.json({statusCode: 400, message: error});
         } else {
-            res.json({statusCode: 200, data: result, message: 'Successful'});
+            res.json({statusCode: 200, data: result, message: 'Success'});
         }
     });
 }
 
-module.exports = {insertCat, getAllCats}
+const deleteCat = (req,res) => {
+    let cat = req.body;
+    model.remove(cat, (error,result) => {
+        if(error) {
+            res.json({statusCode: 400, message: error});
+        } else {
+            res.json({statusCode: 200, data: result, message: 'Successfully removed'});
+        }
+    });
+}
+
+module.exports = {insertCat, getAllCats, deleteCat}
